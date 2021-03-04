a121df98d821db43d1a6a07b8e3d3377ffda36a1
Andres <andres.fernandezcaballero@davinci.edu.ar>
2021-03-04 16:51:51 -0300

    Alpha DesktopApp 1.0.0 *Creada aplicacion de escritorio usando Windows presentation fundation. * solo es funcional el home y el Crud del cliente para ofrecer una demostracion de uso.

---
+++ DesktopApp/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using DesktopApp.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesktopApp
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Main.Content = new HomePage();
+        }
+
+        private void Button_Click_Home_page(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new HomePage();
+        }
+
+        private void Button_Click_Customers_page(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new CustomersPage();
+        }
+
+        private void Button_Click_Orders_page(object sender, RoutedEventArgs e)
+        {
+            // Main.Content = new OrdersPage();
+            MessageBox.Show("Vista no disponible actualemnte.");
+        }
+
+        private void Button_Click_Products_page(object sender, RoutedEventArgs e)
+        {
+            // Main.Content = new ProductsPage();
+            MessageBox.Show("Vista no disponible actualemnte.");
+        }
+    }
+}
